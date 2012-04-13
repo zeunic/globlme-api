4915253cfa63b81d3c1468e759d62b462050209e
Stephen Rivas JR <stephen@zeunic.com>
2012-04-12 23:49:32 -0400

    fuck something is broke LOL

--- modules/stream.js
+++
@@ -1,9 +0,0 @@
-/*
- * Stream.js is used to define functions for retrieving and saving data on the Globl.me Stream.
- * (finish desc later)
- */
-
-exports.getNodeById = function getNodeById(req,res,next) {
-
-}
-
