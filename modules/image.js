76b8a0129db6c1e58a03a4567967a40ec8d04511
Stephen Rivas JR <stephen@zeunic.com>
2012-04-17 21:13:12 -0400

    Wanted to commit the changes to image.js, functionality is done for a solid base in terms of cropping the square of an image after converting it to a quality 80 jpg. Will need to test more on live env in terms of production quality, but its beginning to work decently.

--- modules/image.js
+++ modules/image.js
@@ -1,6 +1,7 @@
 
 var Cloudfiles = require('cloudfiles'),
-	Easyimage = require('easyimage');
+	Easyimage = require('easyimage'),
+	Step = require('step');
 
 var config = { auth: { username: 'zeunicllc', apiKey: 'e4e2973174da5aeb4e63fbdd51f39527' } };
 var cloudfilesClient = Cloudfiles.createClient(config);
@@ -15,38 +16,98 @@ exports.storeImage = function storeImage(sourcePath) {
 };
 
 exports.formatImage = {
-	cropSquare: function(originalSourcePath, offSetX, offSetY){
-		Easyimage.info(originalSourcePath, function(err,stdout,stderr){
-			if(stdout.width > stdout.height) {
-				stdout.width = stdout.height;
-			} else if (stdout.height > stdout.width) {
-				stdout.height = stdout.width;
+	convertImageToJpg: function(originalSourcePath, callback) {
+		Step(
+			function(){
+				Easyimage.convert({
+					src: originalSourcePath,
+					dst: originalSourcePath +'.jpg',
+					quality: 80
+				}, this);
+			},
+			function(){
+				console.log('getting info...');
+				Easyimage.info(originalSourcePath +'.jpg', function(err, imageInfo, errInfo){
+					return callback(err, errInfo, imageInfo, originalSourcePath);
+				});
 			}
+		);
+	},
+	cropSquare: function(imageInfo, originalSourcePath, offSetX, offSetY, callback){
+		if(imageInfo.width > imageInfo.height) {
+			imageInfo.width = imageInfo.height;
+		} else if (imageInfo.height > imageInfo.width) {
+			imageInfo.height = imageInfo.width;
+		}
 
-			Easyimage.thumbnail(
-				{
-					src:originalSourcePath, dst:originalSourcePath + '_square.jpg',
-					width: stdout.width, height: stdout.height,
-					x:offSetX, y:offSetY
-				},
-				function(err, stdout, stderr) {
-					if (err) throw err;
-					console.log('Thumbnail created');
-					console.log(stdout);
-				}
-			);
+		Easyimage.thumbnail(
+			{
+				src: imageInfo.name, dst:originalSourcePath + '_square.jpg',
+				width: imageInfo.width, height: imageInfo.height,
+				x:offSetX, y:offSetY
+			},
+			function(err, stdout, stderr) {
+				if (err) throw err;
+				console.log('easy image info from thumb: ');
+				return callback(arguments);
+			}
+		);
+	},
+	saveImageSizes: function(originalSourcePath){
+		console.log('run after the crop returned?');
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '800.jpg',
+			width: 800
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
 		});
-
-	},
-	saveImageSizes: function(){
-		// Easyimage.rescrop(
-		// 	{},
-		// 	function(err,stdout, stderr){
-
-		// 	}
-		// );
-	},
-	convertJPG: function(){
-
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '640.jpg',
+			width: 640
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
+		});
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '480.jpg',
+			width: 480
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
+		});
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '320.jpg',
+			width: 320
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
+		});
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '200.jpg',
+			width: 200
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
+		});
+		Easyimage.resize({
+			src: originalSourcePath,
+			dst: '80.jpg',
+			width: 80
+		}, function(err, stdout, stderr){
+			if(!err) {
+				console.log(stdout);
+			}
+		});
 	}
 };
