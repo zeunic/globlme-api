9b36caf7918142f4fd4e442fd7698e5f38f63963
Stephen Rivas JR <stephen@zeunic.com>
2012-05-14 02:59:21 -0400

    Moved image.js to begin working with

--- routes/moment.js
+++ routes/moment.js
@@ -1,9 +1,32 @@
 // Module for route handling Moment Creation
 
-module.exports = function (configOptions){
+var Neo4j = require('neo4j'),
+	Step = require('step');
+
+var MomentReferenceNode;
+
+var Moment = function(config){
+
+	db = new Neo4j.GraphDatabase(config.databaseUrl + ':' + config.port);
+	console.log('Moment Module connected: '+config.databaseUrl + ':' + config.port);
+
+	db.query("START n = node(0) MATCH (n) <-[:MOMENTS_REFERENCE]- (moment_ref) RETURN moment_ref", function(errors, nodes) {
+		if (errors) {
+			// TODO: throw errors
+			console.log('Unable to locate a valid reference node for users');
+			console.log(errors);
+		} else {
+			MomentReferenceNode = nodes[0]['moment_ref'];
+			// console.log(MomentReferenceNode);
+		}
+	});
+
 	return {
 		createMoment: function(req,res,next){
 			console.log(req.body, req.files);
 		}
 	};
 };
+
+
+module.exports = Moment;
