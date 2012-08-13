4662eb938889b45e6ad8be04b547678fdfc4a181
Stephen Rivas JR <stephen@zeunic.com>
2012-08-13 02:59:36 -0400

    Attempting to clean up some code for validation and request authorization, etc

--- modules/validate.js
+++ modules/validate.js
@@ -5,13 +5,22 @@
 var crypto = require('crypto'),
 	rc;
 
+
+var validateAuthorization = function(data, auth){
+	var sha256 = crypto.createHash('sha256');
+	sha256.update(data + '3912b4f3-c9ab-4e2f-9007-424b5b1d5de6');
+	var authConfirm = sha256.digest('hex');
+
+	return (authConfirm === auth);
+};
+
 exports.defineRequestAction = function defineRequestAction(req,res,next) {
+	// not using this at the moment
 	next();
 };
 
 exports.validateRequestData = function validateRequestData(req,res,next) {
 	// this data will need flushed out as the API evolves to multiple
-
 	if (req.body.key === "globlme" && req.body.data && req.body.auth) {
 		next();
 	} else {
@@ -21,16 +30,16 @@ exports.validateRequestData = function validateRequestData(req,res,next) {
 	rc = req.body;
 };
 
+exports.checkAuthorization = function checkAuthorization(data, auth) {
+	return validateAuthorization(data, auth);
+};
+
 exports.authorizeRequest = function authorizeRequest(req,res,next){
 	// 3912b4f3-c9ab-4e2f-9007-424b5b1d5de6
-
 	var rc = req.body;
-	var sha256 = crypto.createHash('sha256');
+	var isAuthorized = validateAuthorization(rc.data, rc.auth);
 
-	sha256.update(rc.data + '3912b4f3-c9ab-4e2f-9007-424b5b1d5de6');
-	var authConfirm = sha256.digest('hex');
-
-	if (authConfirm === rc.auth) {
+	if (isAuthorized) {
 		next();
 	} else {
 		// res.writeHead(401, {'Content-Type': 'application/json'});
