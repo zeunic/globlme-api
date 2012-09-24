4445059842fbc3582ab3ac9054aa32113a6a17c4
Stephen Rivas JR <stephen@zeunic.com>
2012-09-24 14:42:02 -0400

    tags/create bug should prevent duplicate tags being created now

--- routes/tags.js
+++ routes/tags.js
@@ -1,6 +1,8 @@
 // util objects & libs
 var Neo4j = require('neo4j'),
 	Step = require('step'),
+	Format = require('format.js'),
+	FormatUtil = new Format(),
 	db,
 	TagReferenceNode;
 
@@ -27,13 +29,22 @@ var Tag = function(config) {
 			var tag = db.createNode({ tag: requestData.tag, date: new Date().getTime() });
 
 			Step(
-				function saveTag(){
-					tag.save(this);
+				function checkTag(){
+					db.getIndexedNode('node_auto_index', 'tag', requestData.tag, this);
 				},
-				function indexTag(){
+				function saveTag(err, results){
+					if(!err && results) {
+						var existingTag = results._data.data;
+						existingTag.id = FormatUtil.graphID( results._data.self );
+						res.json({ status: "success", data: existingTag});
+					} else {
+						tag.save(this);
+					}
+				},/*
+				function indexTag(err, results){
 					tag.index( INDEX_NAME, 'tag', requestData.tag, this.parallel() );
 					tag.index( 'fulltext', 'tag', requestData.tag, this.parallel() );
-				},
+				}, */
 				function relateTagRef(){
 					tag.createRelationshipTo( TagReferenceNode, 'MEMBER_OF', {}, this );
 				},
@@ -41,6 +52,7 @@ var Tag = function(config) {
 					if(!err) {
 						res.json( { status: "success", data: { id: tag.id } } );
 					} else {
+						console.log(err);
 						res.json( { status: "error", message: 'Unable to create tag.' } );
 					}
 				}
